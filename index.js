56c3f33b7ff8fce4c35979f535a2844e68d8e39f
karunya10 <karunya.prabha@gmail.com>
2025-08-20 16:48:59 +0200

    Fixing

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.post("/api/delete-card", deleteCard);
 
 // Start server
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () =>
-  console.log(`🚀 Server running on http://localhost:${PORT}`)
+
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`)
 );
